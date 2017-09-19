fddf52d3118fada18e754b48adc841adb8c57e25
Franz Knupfer <franz@epicodus.com>
2017-09-19 15:43:37 -0700

    update serializer to include edible field

--- app/serializers/tree_serializer.rb
+++ app/serializers/tree_serializer.rb
@@ -1,3 +1,3 @@
 class TreeSerializer < ActiveModel::Serializer
-  attributes :id, :lat, :long, :species, :address, :neighborhood, :scientific_name, :family_name, :genus, :common_name
+  attributes :id, :lat, :long, :species, :address, :neighborhood, :scientific_name, :family_name, :genus, :common_name, :edible
 end
