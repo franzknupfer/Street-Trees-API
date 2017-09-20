97f56379b77374e189c57454899dc5cc0227eb12
Franz Knupfer <franzknupfer@hotmail.com>
2017-09-19 20:55:03 -0700

    add scopes for searching by neighborhood and common_name as well as edibility

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,2 +1,8 @@
 class Tree < ApplicationRecord
+
+  scope :fruit_trees, -> { where ({:edible => "fruit"}) }
+  scope :nut_trees, -> { where ({:edible => "nut"}) }
+  scope :search_by_common_name, -> (common_name) { where("lower(common_name) like ?", (common_name).downcase)}
+  scope :search_by_neighborhood, -> (neighborhood) { where("lower(neighborhood) like ?", (neighborhood).downcase) }
+
 end
