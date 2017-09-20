48a3b697dd5f9a373c14f240aeecbd1b6d26851b
Franz Knupfer <franzknupfer@hotmail.com>
2017-09-19 21:23:49 -0700

    add scopes to search edible trees by neighborhood and common trees by neighborhood

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -4,5 +4,14 @@ class Tree < ApplicationRecord
   scope :nut_trees, -> { where ({:edible => "nut"}) }
   scope :search_by_common_name, -> (common_name) { where("lower(common_name) like ?", (common_name).downcase)}
   scope :search_by_neighborhood, -> (neighborhood) { where("lower(neighborhood) like ?", (neighborhood).downcase) }
+  scope :fruit_or_nut, -> { where ({:edible => ["fruit", "nut"]})}
+
+  def self.edible_by_neighborhood(neighborhood)
+    search_by_neighborhood(neighborhood).fruit_or_nut
+  end
+
+  def self.common_tree_by_neighborhood(neighborhood, common_name)
+    search_by_neighborhood(neighborhood).search_by_common_name(common_name)
+  end
 
 end
