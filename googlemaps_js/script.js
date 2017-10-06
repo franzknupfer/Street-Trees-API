35851e6ade33f216cafab426ea69938659d01147
Franz Knupfer <franzknupfer@hotmail.com>
2017-10-06 10:41:49 -0700

    clean up script

--- googlemaps_js/script.js
+++ googlemaps_js/script.js
@@ -1,89 +1,57 @@
-// function initMap(data) {
-//   let portland = {lat: 45.5231, lng: -122.6765};
-//   let map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 12,
-//     center: portland
-//   });
-//   placeMarkers(data, map);
-// }
-//
-// function addMarker(lat, long, map) {
-//   console.log(long, lat);
-//   let marker = new google.maps.Marker({
-//     position: new google.maps.LatLng(long, lat),
-//     map: map
-//   });
-// }
-//
-// function placeMarkers(data, map) {
-//   data = JSON.parse(data)
-//   data.trees.forEach(function(tree) {
-//     addMarker(parseFloat(tree.lat), parseFloat(tree.long));
-//   });
-// }
-//
-// function treeApiCall(neighborhood, common_name) {
-//   return new Promise(function(resolve, reject) {
-//     let request = new XMLHttpRequest();
-//     let url = `http://localhost:3000/v1/trees?neighborhood=${neighborhood}&common_name=${common_name}`;
-//
-//     request.onload = function() {
-//       if (this.status === 200) {
-//         resolve(request.response);
-//       } else {
-//         reject(Error(request.statusText))
-//       }
-//     }
-//     request.open("GET", url, true);
-//     request.send();
-//   });
-// }
+// Next steps:
+// * Verify that refactored code is working
+// * Find tree icon and use for marker
+// * Set bounds based on trees populated on map
+// * Add info window with further data
+// * Nicer map layer
+// * Add search options beyond neighborhood and type?
+
+
+function treeApiCall(neighborhood, common_name) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `http://localhost:3000/v1/trees?neighborhood=${neighborhood}&common_name=${common_name}`;
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText))
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+}
+
+function initMap(data) {
+  let portland = {lat: 45.5231, lng: -122.6765};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: portland
+  });
+  placeMarkers(data, map);
+}
+
+function placeMarkers(data, map) {
+  data = JSON.parse(data)
+  data.trees.forEach(function(tree) {
+    addMarker(parseFloat(tree.lat), parseFloat(tree.long), map);
+  });
+}
+
+function addMarker(lat, long, map) {
+  let marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, long)
+  });
+  marker.setMap(map);
+}
 
 $(document).ready(function() {
 
   const script = document.createElement('script');
   script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBLjRSAAfFIFfQ1prCr2gDuVWpaIWbZV7U"
   document.body.appendChild(script);
-
-  function initMap(data) {
-    let portland = {lat: 45.5231, lng: -122.6765};
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: portland
-    });
-    placeMarkers(data, map);
-  }
-
-  function addMarker(lat, long, map) {
-    let marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, long)
-    });
-    marker.setMap(map);
-  }
-
-  function placeMarkers(data, map) {
-    data = JSON.parse(data)
-    data.trees.forEach(function(tree) {
-      addMarker(parseFloat(tree.lat), parseFloat(tree.long), map);
-    });
-  }
-
-  function treeApiCall(neighborhood, common_name) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `http://localhost:3000/v1/trees?neighborhood=${neighborhood}&common_name=${common_name}`;
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText))
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
 
   $('#queryParams').click(function() {
     let neighborhood = $('#neighborhood').val();
